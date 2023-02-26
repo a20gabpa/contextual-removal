d119ce30b85379e0e8f6bef96c79eea4dba51399
a20patpe <patrik--pettersson@hotmail.com>
2023-02-26 19:05:43 +0100

    Adding some parameters for Graph function
    Createpdf, Do we save a pdf of the graph.
    relationcutoff, How significatnof a relation is required to be counted as a node(0.5% is default)
    usepercentage, represent edges as percentages(Work in progress)

--- CoOccuranceGraph.py
+++ CoOccuranceGraph.py
@@ -9,7 +9,7 @@ import math
 from StuffObject import StuffObjectNode
 
 class CoOccuranceGraph:
-    def __init__(self, omit):
+    def __init__(self, omit, createpdf = true, relationcutoff = 0.5, usepercentage = true):
         # If we do not have a Co_occurance.txt or IdLookup.txt file yet we will call the function that creates them.
         if not os.path.isfile("./Co_occurance.txt") or not os.path.isfile("./IdLookup.txt"):
             from BuildCoOccurance import CreateCoOccurance
@@ -55,23 +55,23 @@ class CoOccuranceGraph:
                         shouldContinue = True
                 if shouldContinue:
                     continue
-                # Only allow edges with a significant enough weight.
-                if (value / sumCoOccurance) * 100 > 0.5:
-                    fromX.append(i.name)
-                    toY.append(str(lookup[str(key)]))
-                    weight.append((value / sumCoOccurance) * 100)
+                if(usepercentage)
+                    # Only allow edges with a significant enough weight.
+                    if (value / sumCoOccurance) * 100 > relationcutoff:
+                        fromX.append(i.name)
+                        toY.append(str(lookup[str(key)]))
+                        weight.append((value / sumCoOccurance) * 100)
 
         df = pd.DataFrame({'from': fromX, 'to': toY, 'weight': weight})
         G = nx.from_pandas_edgelist(df, 'from', 'to', 'weight')
 
-        # pos = nx.spring_layout(G, seed=1337)
-
         plt.title("Co-Occurance clustering")
         plt.figure(figsize=(150, 150))
         nx.draw_spring(G, node_size=10000, node_color="cyan", with_labels=True)
-
-        plt.savefig('CoOccuranceGraph.pdf')
-
+        
+        if(createpdf):
+            plt.savefig('CoOccuranceGraph.pdf')
+            
         self.nodes = nx.spring_layout(G, seed = 1337)
 
 
