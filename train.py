2adfc7c0daad6f25f42ca09840aff5524abbdcc9
a20patpe <patrik--pettersson@hotmail.com>
2023-03-24 14:42:06 +0100

    Adding subseteuclidian to git.
    Added all the necessary file to train a model. These can be deleted and recreated but for convenience they will be placed here.

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     # Load model
     model = YOLO('yolov8n.yaml')
     print('there!') 
-    results = model.train(data="Z:\Context-CV\coco12800.yaml", epochs=200, device=0, batch= 10)
+    results = model.train(data="Z:\Context-CV\coco12800.yaml", epochs=200, device=0, batch= 10, save_period=10)
     print('General')
     results = model.predict(source="./dataset/coco/images/train2017/000000000257.jpg", save=True, save_txt=True)
     print('Kenobi!')
