ef752ec1bfda2392dca7341fa51b75e5aeb65d2a
Gabriel Gomez Palomäki <102604680+a20gabpa@users.noreply.github.com>
2023-06-04 18:40:13 +0200

    Removed absolute paths

--- train.py
+++ train.py
@@ -7,13 +7,9 @@ from ultralytics import YOLO
 # .\tensYOD\Scripts\activate
 
 if __name__ == '__main__':
-    print('Hello')
     # Load model
     model = YOLO('yolov8n.yaml')
-    print('there!') 
-    results = model.train(data="Z:\Context-CV\coco12800.yaml", epochs=200, device=0, batch= 10, save_period=10)
-    print('General')
+    results = model.train(data="./coco12800.yaml", epochs=200, device=0, batch= 10, save_period=10)
     results = model.predict(source="./dataset/coco/images/train2017/000000000257.jpg", save=True, save_txt=True)
-    print('Kenobi!')
 
     sucess = model.export(format='onnx')
